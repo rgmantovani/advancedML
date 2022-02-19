f4c7553ee5332d5c9882b907cfee646a6651471d
Rafael Gomes Mantovani <rgmantovani@gmail.com>
2022-02-19 18:19:13 -0300

    adding subagging option

--- codes/aula_06_ensembles/bagging.R
+++ codes/aula_06_ensembles/bagging.R
@@ -3,13 +3,19 @@
 # n.learners: number of learners to be used in the committe
 # ----------------------------------------------------------
 
-bagging.train = function(data, n.learners) {
+bagging.train = function(data, n.learners, subagging = FALSE) {
 
     model = list()
     aux = lapply(1:n.learners, function(i) {
 
       # subsampling
-      ids = sample(x = 1:nrow(data), size = nrow(data), replace = TRUE)
+      ids = NULL
+      if(!subagging) {
+        ids = sample(x = 1:nrow(data), size = nrow(data), replace = TRUE)
+      } else {
+        #subagging: sampling with half of the samples, and without replacement
+        ids = sample(x = 1:nrow(data), size = nrow(data)/2, replace = FALSE)
+      }
       sub = data[ids, ]
 
       #fitting a tree ("[[class]] ~ . ")
@@ -31,7 +37,6 @@ bagging.train = function(data, n.learners) {
 # model: fitted model
 # data : testing data, withtout class column
 # ----------------------------------------------------------
-
 bagging.test = function(model, data) {
 
   # generating predictions
@@ -62,24 +67,45 @@ bagging.test = function(model, data) {
 }
 
 # ----------------------------------------------------------
-# testing funcions
+# testing functions
 # ----------------------------------------------------------
 
-# using iris as example
+# using iris as an example
 train = iris[ c(1:30, 51:80, 101:131), ]
 test  = iris[-c(1:30, 51:80, 101:131), ]
 realClass = test$Species
+
+
+# ----------------
+# bagging
+# ----------------
 
 # training model
 model = bagging.train(data = train, n.learners = 10)
 # obtaining prediction
 preds = bagging.test(model = model, data = test[,-ncol(test)])
+preds = factor(preds, levels = levels(realClass))
 
 # computing accuracy
-preds = factor(preds, levels = levels(realClass))
 tab.res = table(realClass,preds)
 acc = sum(diag(tab.res))/sum(tab.res)
 print(acc)
+
+
+# ----------------
+# subagging
+# ----------------
+
+# training subagging model
+model2 = bagging.train(data = train, n.learners = 10, subagging = TRUE)
+# obtaining prediction
+preds2 = bagging.test(model = model2, data = test[,-ncol(test)])
+preds2 = factor(preds2, levels = levels(realClass))
+
+# computing accuracy
+tab.res2= table(realClass,preds2)
+acc2 = sum(diag(tab.res2))/sum(tab.res2)
+print(acc2)
 
 # ----------------------------------------------------------
 # ----------------------------------------------------------
